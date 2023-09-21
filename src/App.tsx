4c772dc85f6ff60bfabbe84d70abfcd62645be23
Emma <emapodlewski@gmail.com>
2023-09-21 09:35:41 -0400

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import BackgroundImage from "/background-portfolio.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <img
+        src={BackgroundImage}
+        className="relative w-full min-h-full bg-center bg-cover  bg-gradient-to-r from-cyan-500 to-blue-500 bg-no-repeat"
+        alt="Coastal Web Development background image"
+      /> */}
+    </>
+  );
+}
+
+export default App;
