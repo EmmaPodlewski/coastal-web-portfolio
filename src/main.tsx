4880fa4562292a814ab1d8d5d78e787ed1a47312
Emma <emapodlewski@gmail.com>
2023-09-29 12:09:35 -0400

    Fix because of react routeur

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import RootLayout from "./components/UI/RootLayout";
 import Expertise from "./components/Expertise";
 import Portfolio from "./components/Portfolio";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      { path: "/emma", element: <Expertise /> },
-      { path: "/alex", element: <Portfolio /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/emma", element: <Expertise /> },
+        { path: "/alex", element: <Portfolio /> },
+      ],
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/coastal-web-portfolio/" }
+);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
