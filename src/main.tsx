a4d806519e4a1edabaa279079354910c3c0e1501
Emma <emapodlewski@gmail.com>
2023-09-29 11:35:13 -0400

    Revert change

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./components/UI/RootLayout";
+import Expertise from "./components/Expertise";
+import Portfolio from "./components/Portfolio";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/emma", element: <Expertise /> },
+      { path: "/alex", element: <Portfolio /> },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
